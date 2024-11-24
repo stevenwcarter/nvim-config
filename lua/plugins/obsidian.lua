9573b538a754dbbe472d1576ad95294a1cb3a53b
Steven Carter <steve@javapl.us>
2024-11-24 16:42:44 -0500

    chore: Removing some comments

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -60,9 +60,6 @@ return {
     ---@param title string|?
     ---@return string
     note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
       local suffix = ""
       if title ~= nil then
         -- If title is given, transform it into valid file name.
